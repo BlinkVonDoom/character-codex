2f14cc81ab4da8caba5898e3e7e6e4fb4d29997c
BlinkVonDoom <blynk.rankin@gmail.com>
2019-03-12 20:29:36 -0600

    - add redirect for unathenicated users - hide edit / update buttons for non creators

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
 
   root to: 'pages#index'
 
-  devise_scope :user do
-    get 'login' => 'devise/sessions#new'
-    get 'signup' => 'devise/registrations#new'
-  end
-
   get '/numenera_character/:id' => 'numenera_characters#show', as: :show_numenera_character
   get '/numenera_character/:id/edit' => 'numenera_characters#edit', as: :edit_numenera_character
   delete '/numenera_character/:id' => 'numenera_characters#destroy', as: :delete_numenera_character
 
   resource :numenera_characters
   resources :numenera_character_skills
+
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new'
+    get 'signup' => 'devise/registrations#new'
+  end
 end
