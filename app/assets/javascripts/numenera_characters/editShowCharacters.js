c3fa3d535608b49e0cfd0f773916e94808c2524a
blinkvondom <blynk.rankin@gmail.com>
2019-04-23 15:42:54 -0600

    update js for edit character

---
+++ app/assets/javascripts/numenera_characters/editShowCharacters.js
@@ -0,0 +1,11 @@
+$(document).on('ready', () => {
+    let newSkill  = document.getElementById('new_numenera_character_skill')
+    let editSkill = document.getElementById('edit-numenera-character-skill')
+    $("#add-numenera-character-skill-button").click(() => {
+        newSkill.style.display = "block";
+    });
+    
+    $("#edit-numenera-character-skill-button").click(() => {
+       editSkill.style.display = "block";
+    });
+})
