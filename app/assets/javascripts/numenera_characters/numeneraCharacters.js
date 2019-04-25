eea81af599954d13d711edfb2416a6ca8b21b18d
blinkvondom <blynk.rankin@gmail.com>
2019-04-25 14:16:32 -0600

    add buttons and styling

--- app/assets/javascripts/numenera_characters/numeneraCharacters.js
+++ app/assets/javascripts/numenera_characters/numeneraCharacters.js
@@ -1,22 +1,38 @@
 $(document).on('turbolinks:load', () => {
     let characterSkills = () => {
         let newEsotery = document.getElementById('add-numenera-esotery');
-        let newSkill  = document.getElementById('new_numenera_character_skill');
+        let newSkill = document.getElementById('add-numenera-character-skill');
 
         let editEsoteryBtn = document.getElementsByClassName('edit-numenera-esotery-button');
-        let editEsotery = document.getElementsByClassName('edit-numenera-esotery');
-        let editSkillBtn = document.getElementsByClassName('edit-numenera-character-skill-button')
-        let editSkill = document.getElementsByClassName('edit-numenera-character-skill')
+        let editSkillBtn = document.getElementsByClassName('edit-numenera-character-skill-button');
 
-        for(let i=0; i< editEsoteryBtn.length; i++){
+        let cancelEsotryBtn = document.getElementsByClassName('cancel-esotery-edit');
+        let cancelSkillBtn = document.getElementsByClassName('cancel-skill-edit');
+        let editEsotery = document.getElementsByClassName('edit-numenera-esotery');
+        let editSkill = document.getElementsByClassName('edit-numenera-character-skill');
+
+        for (let i = 0; i < editEsoteryBtn.length; i++) {
             editEsoteryBtn[i].addEventListener('click', () => {
-                editEsotery[i].style.display = 'block'
+                editEsotery[i].style.display = 'block';
             })
         }
 
-        for(let i = 0; i < editSkillBtn.length; i++) {
+        for (let i = 0; i < editSkillBtn.length; i++) {
             editSkillBtn[i].addEventListener('click', () => {
                 editSkill[i].style.display = 'block';
+            })
+        }
+
+        for(let i = 0; i < cancelEsotryBtn.length; i++) {
+            cancelEsotryBtn[i].addEventListener('click', () => {
+                editEsotery[i].style.display = 'none';
+            })
+        }
+
+        for (let i = 0; i < cancelSkillBtn.length; i++) {
+            cancelSkillBtn[i].addEventListener('click', () => {
+                console.log('click', i)
+                editSkill[i].style.display = 'none';
             })
         }
 
@@ -26,9 +42,17 @@ $(document).on('turbolinks:load', () => {
 
 
         $('#add-numenera-esotery-button').click(() => {
-            newEsotery.style.display = "block"
+            newEsotery.style.display = "block";
         })
-    }
+
+        $('#cancel-add-skill').click(() => {
+            newSkill.style.display = 'none';
+        })
+
+        $('#cancel-add-esotery').click(() => {
+            newEsotery.style.display = 'none';
+        })
+    }   
 
     characterSkills();
 })
