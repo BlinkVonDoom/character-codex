eee7e22b91538b5c32843c8ef7e4d40e535f8d6e
blinkvondom <blynk.rankin@gmail.com>
2019-05-24 13:18:03 -0600

    deploy changes

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module CharacterCodex
 
     config.action_controller.include_all_helpers = false
 
+    config.eager_load_paths << "#{Rails.root}/lib"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
